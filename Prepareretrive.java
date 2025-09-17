c5f502274c22f430bb793b13d943c21f73cf663f
R.Hemanaganjali <ravurihema35@gmail.com>
2025-09-17 19:44:19 +0530

    Add files via upload

---
+++ Prepareretrive.java
@@ -0,0 +1,13 @@
+import java.sql.*;
+import  java.util.*;
+class Prepareretrive{
+public static viod mai(String args[]){
+Class.forName("com.mysql.cj.jdbc.Driver");
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the name");
+String name=sc.nextLine();
+System.out.println("enter the password");
+String pwd=sc.nextLine();
+
+
+
